40509fcab8c5b77560c5482a569b9a267781b85b
Supheria <Supheria@gmail.com>
2024-05-09 19:55:15 +0800

    remove Hoi4ScriptParser

--- SimpleScriptSerialization/Program.cs
+++ SimpleScriptSerialization/Program.cs
@@ -1,79 +1,79 @@
-using Hoi4ScriptParser;
-using Hoi4ScriptParser.Data;
+using LocalUtilities.FileUtilities;
+using LocalUtilities.SimpleScript.Data;
+using LocalUtilities.SimpleScript.Serialization;
+using LocalUtilities.StringUtilities;
 using System.Text;
 
 namespace SimpleScriptSerialization
 {
     internal static class Program
     {
-        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
-        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
-        static extern bool AllocConsole();
-
-        [System.Runtime.InteropServices.DllImport("Kernel32")]
-        public static extern void FreeConsole();
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            var analyzer = new Analyzer();
-            var tokens = analyzer.Parse("test.md");
-            var log = analyzer.ErroLog;
-            AllocConsole();   //开启控制台
-            var message = new StringBuilder();
-            foreach (var token in tokens)
-                message.Append(token.ToString());
-            Console.Write(message);
+            var data = new FormData() { Datas = [new(), new()] };
+            //var a = new FormDataSerialization() { Source = data }.SaveToFile();
+            var b = new FormDataSerialization().LoadFromFile(out var m);
+
             Application.Run(new Form1());
         }
     }
 
-    static class Shell
+    public class FormData
     {
-        /// <summary>  
-        /// 输出信息  
-        /// </summary>  
-        /// <param name="format"></param>  
-        /// <param name="args"></param>  
-        public static void WriteLine(string message, ConsoleColor GetConsoleColor)
+        public Size MinimumSize { get; set; }
+
+        public virtual Size Size { get; set; }
+
+        public virtual Point Location { get; set; }
+
+        public virtual FormWindowState WindowState { get; set; } = FormWindowState.Normal;
+
+        public virtual int Padding { get; set; } = 12;
+
+        public List<FormData> Datas { get; set; } = [];
+    }
+
+    public class FormDataSerialization : SsSerialization<FormData>
+    {
+        public FormDataSerialization() : base(new())
         {
-            Console.ForegroundColor = GetConsoleColor;
-            Console.WriteLine(@"[{0}]{1}", DateTimeOffset.Now, message);
+            OnSerialize += FormData_Serialize;
+            OnDeserialize += FormData_Deserialize;
         }
 
-        /// <summary>  
-        /// 输出信息  
-        /// </summary>  
-        /// <param name="format"></param>  
-        /// <param name="args"></param>  
-        public static void WriteLine(string format, params object[] args)
+        public override string LocalName => nameof(FormData);
+
+        private void FormData_Serialize(SsSerializer serializer)
         {
-            WriteLine(string.Format(format, args));
+            serializer.WriteTag(nameof(Source.MinimumSize), (Source.MinimumSize.Width, Source.MinimumSize.Height).ToArrayString());
+            serializer.WriteTag(nameof(Source.Location), (Source.Location.X, Source.Location.Y).ToArrayString());
+            serializer.WriteTag(nameof(Source.WindowState), Source.WindowState.ToString());
+            serializer.WriteTag(nameof(Source.Padding), Source.Padding.ToString());
+            Source.Datas.Serialize(serializer, new FormDataSerialization());
         }
 
-        /// <summary>  
-        /// 输出信息  
-        /// </summary>  
-        /// <param name="output"></param>  
-        public static void WriteLine(string output)
+        private void FormData_Deserialize(Token token)
         {
-            Console.ForegroundColor = GetConsoleColor(output);
-            Console.WriteLine(@"[{0}]{1}", DateTimeOffset.Now, output);
-        }
-
-        /// <summary>  
-        /// 根据输出文本选择控制台文字颜色  
-        /// </summary>  
-        /// <param name="output"></param>  
-        /// <returns></returns>  
-        private static ConsoleColor GetConsoleColor(string output)
-        {
-            if (output.StartsWith("警告")) return ConsoleColor.Yellow;
-            if (output.StartsWith("错误")) return ConsoleColor.Red;
-            if (output.StartsWith("注意")) return ConsoleColor.Green;
-            return ConsoleColor.Gray;
+            if (token is TagValues tagValues)
+            {
+                if (token.Name is nameof(Source.MinimumSize))
+                    Source.MinimumSize = tagValues.Tag.ToSize(Source.MinimumSize);
+                else if (token.Name is nameof(Source.Location))
+                    Source.Location = tagValues.Tag.ToPoint(Source.Location);
+                else if (token.Name is nameof(Source.WindowState))
+                    Source.WindowState = tagValues.Tag.ToEnum<FormWindowState>();
+                else if (token.Name is nameof(Source.Padding))
+                    Source.Padding = tagValues.Tag.ToInt(Source.Padding);
+            }
+            else if (token is Scope scope)
+            {
+                if (token.Name is nameof(FormData))
+                    Source.Datas.Add(SsDeserializeTool.Deserialize(new FormDataSerialization(), scope));
+            }
         }
     }
 }
